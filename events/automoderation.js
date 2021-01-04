120a1b38732f744c826c42ed487c4f4419d741de
EnotKEK3 <57226653+enotkek@users.noreply.github.com>
2021-01-04 02:27:50 +0200

    НАХУЙ ВАШЕ ООП

--- events/automoderation.js
+++ events/automoderation.js
@@ -1,39 +1,39 @@
-const inviteRegex = () => {
-    const protocol = '(?:(?:http|https)://)?';
-    const subdomain = '(?:www.)?';
-    const domain = '(?:disco|discord|discordapp).(?:com|gg|io|li|me|net|org)';
-    const path = '(?:/(?:invite))?/([a-z0-9-.]+)';
+// const inviteRegex = () => {
+//     const protocol = '(?:(?:http|https)://)?';
+//     const subdomain = '(?:www.)?';
+//     const domain = '(?:disco|discord|discordapp).(?:com|gg|io|li|me|net|org)';
+//     const path = '(?:/(?:invite))?/([a-z0-9-.]+)';
 
-    const regex = `(${protocol}${subdomain}(${domain}${path}))`;
+//     const regex = `(${protocol}${subdomain}(${domain}${path}))`;
 
-    return new RegExp(regex, 'i');
-};
+//     return new RegExp(regex, 'i');
+// };
 
-async function inviteCheck(client, message) {
-    if (!message.member.hasPermission('ADMINISTRATOR') && message.channel.permissionsFor(client.user.id).has('MANAGE_MESSAGES')) {
-        const check = inviteRegex().test(message.content);
-        if (check) {
-            const fetchInvite = await client.fetchInvite(message.content).catch(null);
-            if (fetchInvite.guild.id === message.guild.id) return false;
+// async function inviteCheck(client, message) {
+//     if (!message.member.hasPermission('ADMINISTRATOR') && message.channel.permissionsFor(client.user.id).has('MANAGE_MESSAGES')) {
+//         const check = inviteRegex().test(message.content);
+//         if (check) {
+//             const fetchInvite = await client.fetchInvite(message.content).catch(null);
+//             if (fetchInvite.guild.id === message.guild.id) return false;
 
-            if (message.channel.permissionsFor(client.user.id).has('MANAGE_MESSAGES')) {
-                await message.delete().catch(null);
-            }
+//             if (message.channel.permissionsFor(client.user.id).has('MANAGE_MESSAGES')) {
+//                 await message.delete().catch(null);
+//             }
 
-            message.channel.send(`${message.author} рекламирует свой сервер!`);
-            return true;
-        }
-        return false;
-    }
-    return false;
-}
+//             message.channel.send(`${message.author} рекламирует свой сервер!`);
+//             return true;
+//         }
+//         return false;
+//     }
+//     return false;
+// }
 
-module.exports = client => {
-    client.on("message", async message => {
-        if (message.author.bot) return;
+// module.exports = client => {
+//     client.on("message", async message => {
+//         if (message.author.bot) return;
 
-        if (message.channel.type === 'dm') return;
+//         if (message.channel.type === 'dm') return;
 
-        if (await inviteCheck(client, message)) return;
-    })
-}
+//         if (await inviteCheck(client, message)) return;
+//     })
+// }
