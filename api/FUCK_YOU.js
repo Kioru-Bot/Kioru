6cfe62e238ba995794ee0267c1af7d48fa657329
EnotKEK3 <57226653+enotkek@users.noreply.github.com>
2021-01-19 19:59:12 +0300

    Я убил Ifeauss

--- api/FUCK_YOU.js
+++ api/FUCK_YOU.js
@@ -10,4 +10,17 @@ stats.get("/stats", ((req, res) => {
     })
 }))
 
+stats.get("/stats", ((req, res) => {
+    res.json({
+        guilds: client.guilds.cache.size,
+        channels: client.channels.cache.size,
+        users: client.users.cache.size
+    })
+}))
+stats.get("/guilds", ((req, res) => {
+    res.json({
+        guilds: client.guilds.cache.map(guild => guild.id),
+    })
+}))
+
 module.exports = stats
